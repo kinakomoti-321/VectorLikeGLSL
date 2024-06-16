0c6e0bf23efdcb3a28d5c18021664ca50801746c
kinakomoti <kinakomoti743@gmail.com>
2024-06-16 15:54:17 +0900

    Add : mat2

--- VectorLikeGLSL/VectorLikeGLSL/include/Matrix/matrix2x2.h
+++ VectorLikeGLSL/VectorLikeGLSL/include/Matrix/matrix2x2.h
@@ -3,7 +3,28 @@
 #include "../Vector/vector.h"
 
 namespace VectorLikeGLSL {
-	struct mat2x2 {
+	struct mat2 {
 		vec2 m[2];
+
+		mat2(const float a) {
+			m[0] = vec2(a);
+			m[1] = vec2(a);
+		}
+
+		mat2(const float a, const float b, const float c, const float d) {
+			m[0] = vec2(a, b);
+			m[1] = vec2(c, d);
+		}
+
+		mat2(const vec2& a, const vec2& b) {
+			m[0] = a;
+			m[1] = b;
+		}
+
+		mat2(const mat2& a) {
+			m[0] = a.m[0];
+			m[1] = a.m[1];
+		}
 	};
+
 }
